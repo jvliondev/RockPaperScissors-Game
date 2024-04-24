d807883af9553480c565418582f634ceb32f84e8
Julian Vasquez <104545706+jvliondev@users.noreply.github.com>
2024-04-24 12:23:54 -0500

    Refactored
    There were changes to the code. Input text removed. Added three buttons for choices.

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ let playerSelection;
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
-
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
 
 /*Below this getComputerChoice function is randomizing a number 
 depending on the variable options length and math.floor is ensuring
@@ -23,8 +25,9 @@ it is a whole number. Since the array length contains 3 items,
 the function randomly gets a number which is connected to the position
 of the options index. This gives the choice between rock, paper, or scissors.
 */
+
 function getComputerChoice() {
-    let options = ['rock', 'paper', 'scissors'];
+    let options = [rock, paper, scissors];
     let index = Math.floor(Math.random() * options.length);
     return options[index];
 }
@@ -32,34 +35,79 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection){
 
-    if (computerSelection === 'paper' && playerSelection == "rock"){
+    if (computerSelection === paper && playerSelection == rock){
         return "You lose! Paper beats Rock!"
-    } else if (computerSelection === 'paper' && playerSelection == "scissors"){
+    } else if (computerSelection === paper && playerSelection == scissors){
         return "You win! Scissors beat Paper!"
-    } else if (computerSelection === 'paper' && playerSelection == "paper"){
+    } else if (computerSelection === paper && playerSelection == paper){
         return "Draw! Nobody Wins!"
-    } else if (computerSelection === 'scissors' && playerSelection == "rock"){
+    } else if (computerSelection === scissors && playerSelection == rock){
         return "You win! Rock beats Scissors!"
-    } else if (computerSelection === 'scissors' && playerSelection == "paper"){
+    } else if (computerSelection === scissors && playerSelection == paper){
         return "You lose! Scissors beats Paper!"
-    } else if (computerSelection === 'scissors' && playerSelection == "scissors"){
+    } else if (computerSelection === scissors && playerSelection == scissors){
         return "Draw! Nobody Wins!"
-    } else if (computerSelection === 'rock' && playerSelection == "paper"){
+    } else if (computerSelection === rock && playerSelection == paper){
         return "Your win! Paper beats Rock!"
-    } else if (computerSelection === 'rock' && playerSelection == "scissors"){
+    } else if (computerSelection === rock && playerSelection == scissors){
         return "You lose! Rock beats Scissors!"
-    } else if (computerSelection === 'rock' && playerSelection == "rock"){
+    } else if (computerSelection === rock && playerSelection == rock){
         return "Draw! Nobody Wins!"
     } else {
         return "Please enter rock, paper or scissors!"
     }
 }
 
-document.getElementById('myButton').addEventListener('click', function() {
-    playerSelection = document.getElementById('myInput').value.toLowerCase();
+document.getElementById('rock').addEventListener('click', function() {
+    playerSelection = rock;
     computerSelection = getComputerChoice();
     let gameResult = playRound(playerSelection, computerSelection);
     document.getElementById('results').innerHTML = gameResult;
+
+    let win = gameResult.includes("win");
+    let lose = gameResult.includes("lose");
+// link the results to the playerScore and computerScore
+//if the computer wins, add 1 to the computerScore
+//if the player wins, add 1 to the playerScore
+    if (win){
+        playerScore += 1;
+        document.getElementById("playerScore").innerHTML = playerScore;
+    }
+    if (lose){
+        computerScore += 1;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    }
+});
+
+
+
+document.getElementById('paper').addEventListener('click', function() {
+    playerSelection = paper;
+    computerSelection = getComputerChoice();
+    let gameResult = playRound(playerSelection, computerSelection);
+    document.getElementById('results').innerHTML = gameResult;
+
+    let win = gameResult.includes("win");
+    let lose = gameResult.includes("lose");
+// link the results to the playerScore and computerScore
+//if the computer wins, add 1 to the computerScore
+//if the player wins, add 1 to the playerScore
+    if (win){
+        playerScore += 1;
+        document.getElementById("playerScore").innerHTML = playerScore;
+    }
+    if (lose){
+        computerScore += 1;
+        document.getElementById("computerScore").innerHTML = computerScore;
+    }
+});
+
+document.getElementById('scissors').addEventListener('click', function() {
+     playerSelection = scissors;
+     computerSelection = getComputerChoice();
+     let gameResult = playRound(playerSelection, computerSelection);
+     document.getElementById('results').innerHTML = gameResult;
+
    //------------------------------------------------------------------- 
     let win = gameResult.includes("win");
     let lose = gameResult.includes("lose");
